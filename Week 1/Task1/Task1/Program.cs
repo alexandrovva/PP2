3cf3198a8220b5e677b6102469d21883b9144ec9
Anastassiya Nikitina <definix@MacBook-Anastassiya.local>
2019-01-26 10:49:18 +0600

    test commit

--- Week 1/Task1/Task1/Program.cs
+++ Week 1/Task1/Task1/Program.cs
@@ -6,7 +6,7 @@ namespace Task1
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("abc");
         }
     }
 }
