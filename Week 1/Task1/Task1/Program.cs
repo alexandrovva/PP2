eb15eda42cde913da8ad889ca77da239c3779c2f
Anastassiya Nikitina <definix@MacBook-Anastassiya.local>
2019-01-26 11:52:34 +0600

    commnent

--- Week 1/Task1/Task1/Program.cs
+++ Week 1/Task1/Task1/Program.cs
@@ -5,8 +5,29 @@ namespace Task1
     class MainClass
     {
         public static void Main(string[] args)
-        {
-            Console.WriteLine("abc");
+        { 
+            int x = int.Parse(Console.ReadLine());
+            int[] a = new int[x];
+            int[] a1 = new int[x];
+            string[] arr = Console.ReadLine().Split();
+            for (int i = 0; i < x; i++)
+            {
+
+             
+                a[i] = int.Parse(arr[i]);
+            }
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 2; j < a[i]; j++)
+                {
+                    if (a[i]%j!=0)
+                    { 
+                    a1[i] = a[i];
+                    }
+                }
+            }
+            foreach(int k in a1)
+            Console.Write(k);
         }
     }
 }
