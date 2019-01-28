a9b70c9e1814063728a92dc7ad0a71037695a354
Anastassiya Nikitina <definix@MacBookstassiya.WORKGROUP>
2019-01-28 15:47:08 +0600

    commnent

--- Week 1/Task1/Task1/Program.cs
+++ Week 1/Task1/Task1/Program.cs
@@ -7,43 +7,43 @@ namespace Task1
     {
         public static void Main(string[] args)
         {
-            int c = 0;
-            int x = int.Parse(Console.ReadLine());
-            int[] a = new int[x];
-            List<int> l = new List<int>();
-            string[] s = Console.ReadLine().Split();
+            int c = 0; //счетчик простых чисел
+            int x = int.Parse(Console.ReadLine()); //число элементов, которые далее будут вводиться
+            int[] a = new int[x]; //массив для введенных чисел
+            List<int> l = new List<int>(); //лист для простых чисел
+            string[] s = Console.ReadLine().Split(); //вводим строку с числами, сразу разделяя их при считывании строки
 
             for (int i = 0; i < x; i++)
             {
-                a[i] = int.Parse(s[i]);
+                a[i] = int.Parse(s[i]); //переводим из строки в int и добавляем в массив
             }
 
             for (int i = 0; i < a.Length; i++)
             {
-                bool k = true;
-                if (a[i] == 1)
+                bool k = true; //переменная для проверки в конце цикла: простое число или нет
+                if (a[i] == 1) //единица сразу исключается
                 {
                     k = false;
                 }
                 for (int j = 2; j < a[i]; j++)
                 {
-                    if (a[i] % j == 0 && a[i] != j)
+                    if (a[i] % j == 0 && a[i] != j) //если число в остатке при делении не на себя дает 0, оно не простое
                     {
-                        k = false;
+                        k = false; //число уже не простое, поэтому выходим из цикла
                         break;
                     }
                 }
 
-                if (k == true)
+                if (k == true) //если число прошло все проверки и переменная осталась true, число простое
                 {
-                    l.Add(a[i]);
-                    c++;
+                    l.Add(a[i]); //добавляем данное простое число в лист
+                    c++; //увеличиваем счетчик простых чисел 
                 }
             }
 
 
-            Console.WriteLine(c);
-            foreach (int i in l)
+            Console.WriteLine(c); //выводим кол-во простых чисел
+            foreach (int i in l) //пробегаемся по листу и выводим все содержимое через пробел
             {
                 Console.Write(i+" ");
 
