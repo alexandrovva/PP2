891b4357124673299f0f0b38eb52661ed5d525d7
Anastassiya Nikitina <definix@MacBookstassiya.WORKGROUP>
2019-01-28 16:36:17 +0600

    commnent

--- Week 1/Task4/Task4/Program.cs
+++ Week 1/Task4/Task4/Program.cs
@@ -6,14 +6,14 @@ namespace Task4
     {
         public static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            for (int i = 1; i <= n; i++)
+            int n = int.Parse(Console.ReadLine()); //число рядов
+            for (int i = 1; i <= n; i++) //ряд
             {
-                for (int j = 1; j <= i; j++)
+                for (int j = 1; j <= i; j++) //кол-во звездочек в ряду
                 {
                     Console.Write("[*]");
                 }
-                Console.WriteLine();
+                Console.WriteLine(); //переход на следующий ряд
             }
         }
     }
