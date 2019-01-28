3b9a2a8422f152d012efd3fb14f302c28eabc7aa
Anastassiya Nikitina <definix@MacBookstassiya.WORKGROUP>
2019-01-28 16:18:07 +0600

    commnent

--- Week 1/Task3/Task3/Program.cs
+++ Week 1/Task3/Task3/Program.cs
@@ -6,21 +6,21 @@ namespace Task3
     {
         public static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            int[] a = new int[n];
-            int[] a1 = new int[2 * n];
-            string[] s = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine()); //количество чисел сразу переведенных из строки в int
+            int[] a = new int[n]; //массив для введенных чисел
+            int[] a1 = new int[2 * n]; //массив с удвоенными цифрами
+            string[] s = Console.ReadLine().Split(); //вводим числа как строку, сразу разделяя
 
             for(int i = 0; i < n; i++)
             {
-                a[i] = int.Parse(s[i]);
+                a[i] = int.Parse(s[i]); //преобразуем числа из строки в int и записываем в массив
             }
-            int x = 0;
+            int x = 0; //для продвижения по второму массиву, где все удвоенно
             for(int j = 0; j < a.Length; j++)
             {
-                a1[x] = a[j];
-                a1[x+1] = a[j];
-                x += 2;
+                a1[x] = a[j]; //0-ой элемент первого массива сталовится 1
+                a1[x+1] = a[j]; //1-ый элемент становится 1
+                x += 2; //следующие места для чисел нового массива: 2 и 3
             }
             for(int i = 0; i < a1.Length; i++)
             {
