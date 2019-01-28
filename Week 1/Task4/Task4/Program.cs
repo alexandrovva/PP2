04f323d6dd11a410f305b2f34c54dbab384532f6
Anastassiya Nikitina <definix@MacBookstassiya.WORKGROUP>
2019-01-28 16:26:00 +0600

    commnent

---
+++ Week 1/Task4/Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("[*]");
+            }
+        }
+    }
+}
