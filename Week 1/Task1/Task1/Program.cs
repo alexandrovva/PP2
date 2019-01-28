312921824e256b77e8ea09bbf83c6a2a5f85559e
Anastassiya Nikitina <definix@MacBookstassiya.WORKGROUP>
2019-01-28 15:34:14 +0600

    commnent

--- Week 1/Task1/Task1/Program.cs
+++ Week 1/Task1/Task1/Program.cs
@@ -1,33 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Task1
 {
     class MainClass
     {
         public static void Main(string[] args)
-        { 
+        {
+            int c = 0;
             int x = int.Parse(Console.ReadLine());
             int[] a = new int[x];
-            int[] a1 = new int[x];
-            string[] arr = Console.ReadLine().Split();
+            List<int> l = new List<int>();
+            string[] s = Console.ReadLine().Split();
+
             for (int i = 0; i < x; i++)
             {
-
-             
-                a[i] = int.Parse(arr[i]);
+                a[i] = int.Parse(s[i]);
             }
+
             for (int i = 0; i < a.Length; i++)
             {
+                bool k = true;
+                if (a[i] == 1)
+                {
+                    k = false;
+                }
                 for (int j = 2; j < a[i]; j++)
                 {
-                    if (a[i]%j!=0)
-                    { 
-                    a1[i] = a[i];
+                    if (a[i] % j == 0 && a[i] != j)
+                    {
+                        k = false;
+                        break;
                     }
                 }
+
+                if (k == true)
+                {
+                    l.Add(a[i]);
+                    c++;
+                }
             }
-            foreach(int k in a1)
-            Console.Write(k);
+
+
+            Console.WriteLine(c);
+            foreach (int i in l)
+            {
+                Console.Write(i+" ");
+
+            }
+
         }
     }
 }
+
+
