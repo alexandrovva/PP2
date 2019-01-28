771bc762762294a9fdfc7458dd232566a249d721
Anastassiya Nikitina <definix@MacBookstassiya.WORKGROUP>
2019-01-28 16:10:23 +0600

    commnent

---
+++ Week 1/Task3/Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            int[] a1 = new int[2 * n];
+            string[] s = Console.ReadLine().Split();
+
+            for(int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(s[i]);
+            }
+            int x = 0;
+            for(int j = 0; j < a.Length; j++)
+            {
+                a1[x] = a[j];
+                a1[x+1] = a[j];
+                x += 2;
+            }
+            for(int i = 0; i < a1.Length; i++)
+            {
+                Console.Write(a1[i] + " ");
+            }
+        }
+    }
+}
