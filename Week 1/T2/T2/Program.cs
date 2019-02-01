595bbd1783972fc46a6ffe16a6002cea36abcbce
Anastassiya Nikitina <definix@MacBookstassiya.WORKGROUP>
2019-02-01 23:52:05 +0600

    commnent

--- Week 1/T2/T2/Program.cs
+++ Week 1/T2/T2/Program.cs
@@ -17,16 +17,16 @@ namespace T2
     {
         static void Main(string[] args)
         {
-            Student st = new Student();
-            string s1 = Console.ReadLine();
+            Student st = new Student(); //конструктор
+            string s1 = Console.ReadLine(); 
             string s2 = Console.ReadLine();
             string s3 = Console.ReadLine();
             st.name = s1;
             st.id = s2;
-            int x = int.Parse(s3);
+            int x = int.Parse(s3); //переводим строку, чтобы придавить +1 год
             x = x + 1;
             st.year = x;
-            st.PrintInfo();
+            st.PrintInfo(); //вызов вывода
         }
     }
 }
