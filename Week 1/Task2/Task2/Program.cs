e75de562dd2fa2ca7de937187289d38f34afc692
Anastassiya Nikitina <definix@MacBook-Anastassiya.local>
2019-02-01 13:05:44 +0600

    commnent

---
+++ Week 1/Task2/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task2
+{
+    class Student
+    {
+        public string name;
+        public string surname;
+        public int year;
+
+        public Student(string name,string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+    }
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Student st2 = new Student("Anastassiya", "Nikitina");
+            st2.year = 1;
+            Console.WriteLine(st2);
+        }
+    }
+}
