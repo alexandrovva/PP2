de42e4174c7dfdda32eaf996fd2193f981516b1f
Alexandrovva <definix@MacBookstassiya.WORKGROUP>
2019-02-04 16:44:37 +0600

    commnent

--- Week 2/Task1/Task1/Program.cs
+++ Week 2/Task1/Task1/Program.cs
@@ -5,20 +5,20 @@ namespace Task1
 {
     class MainClass
     {
-        static string Palindrome(string s,int i,int j)
+        static string Palindrome(string s,int i,int j) //рекурсия для проверки палиндрома
         {
-            if (i > j) return "Yes";
-            if (s[i] != s[j]) return "No";
-            return Palindrome(s, i + 1, j - 1);
+            if (i > j) return "Yes"; //если указатели перешели друг друга, то это палиндром
+            if (s[i] != s[j]) return "No"; 
+            return Palindrome(s, i + 1, j - 1); //сдвигаем указатели вправл и влево соответственно
 
         }
         public static void Main(string[] args)
         {
-            StreamReader st = new StreamReader("input.txt");
-            string s = st.ReadLine();
+            StreamReader st = new StreamReader("input.txt"); 
+            string s = st.ReadLine(); //считываем
 
             StreamWriter sw = new StreamWriter("output.txt");
-            sw.WriteLine(Palindrome(s, 0, s.Length - 1));
+            sw.WriteLine(Palindrome(s, 0, s.Length - 1)); //переход к функции-рекурсии
             sw.Close();
         }
     }
